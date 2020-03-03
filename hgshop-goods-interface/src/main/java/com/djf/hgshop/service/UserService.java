1fcf185b30a53bb297484102d10dfabe71edc7d8
dong jiang fan <835996356@qq.com>
2020-03-03 17:09:18 +0800

    https://github.com/dongjiangfan/2020.3.3.git

---
+++ hgshop-goods-interface/src/main/java/com/djf/hgshop/service/UserService.java
@@ -0,0 +1,13 @@
+package com.djf.hgshop.service;
+
+/**
+ * 
+ * @ClassName: UserService 
+ * @Description: TODO
+ * @author: 董江帆
+ * @date: 2020年3月3日 下午1:35:46
+ */
+public interface UserService {
+
+	boolean login(String userName,String passWord);
+}
